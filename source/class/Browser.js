46de71a49c8da3ff776e1c95f7ac2846289f7256
Sebastian Werner <info@sebastian-werner.net>
2012-02-21 10:33:34 +0100

    More styling/icons work

--- source/class/Browser.js
+++ source/class/Browser.js
@@ -27,11 +27,6 @@ core.Class('api.Browser', {
 		document.body.appendChild(this.__treeElem);
 		document.body.appendChild(this.__contentElem);
 
-		// Feature Test
-		if (core.bom.Style.property("maskImage")) {
-			document.documentElement.className += " maskImage";
-		}
-
 		// Load stylesheet
 		core.io.StyleSheet.load(core.io.Asset.toUri("api/reset.css"));
 		core.io.StyleSheet.load(core.io.Asset.toUri("api/style.css"));
