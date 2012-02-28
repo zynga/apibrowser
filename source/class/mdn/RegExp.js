12447dd1861cee02cc5488b4bba130ae7af28fad
Sebastian Werner <info@sebastian-werner.net>
2012-02-28 18:35:51 +0100

    Added test for native types

---
+++ source/class/mdn/RegExp.js
@@ -0,0 +1,22 @@
+/** 
+ * Creates a regular expression object for matching text with a @pattern {String} and optional @flags {String}. 
+ */
+core.Main.declareNamespace("RegExp", function(pattern, flags) {
+	
+});
+
+core.Main.addMembers("RegExp", {
+	
+	/** Executes a search for a match in its @string {String} parameter. */
+	exec: function(string) {},
+
+	/** Tests for a match in its @string {String} parameter. */
+	test: function(string) {},
+
+	/** Returns an object literal representing the specified object; you can use this value to create a new object. */
+	toSource: function() {},
+
+	/** Returns a string representing the specified object. */
+	toString: function() {},
+	
+});
