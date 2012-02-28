00d50a2ec1c421324df88c43bc1e4c4f95e6fecb
Sebastian Werner <info@sebastian-werner.net>
2012-02-28 18:40:58 +0100

    Added properties

--- source/class/mdn/RegExp.js
+++ source/class/mdn/RegExp.js
@@ -7,6 +7,21 @@ core.Main.declareNamespace("RegExp", function(pattern, flags) {
 
 core.Main.addMembers("RegExp", {
 	
+	/** {=Boolean} Whether to test the regular expression against all possible matches in a string, or only against the first. */
+	global: null,
+	
+	/** {=Boolean} Whether to ignore case while attempting a match in a string. */
+	ignoreCase: null,
+
+	/** {=Integer} The index at which to start the next match. */
+	lastIndex: null,
+
+	/** {=Boolean} Whether or not to search in strings across multiple lines. */
+	multiline: null,
+
+	/** {=String} The text of the pattern. */
+	source: null,
+	
 	/** Executes a search for a match in its @string {String} parameter. */
 	exec: function(string) {},
 
