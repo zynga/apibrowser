afbac2f2000ccab68f2e73eece95743d024baa8a
Sebastian Werner <info@sebastian-werner.net>
2012-03-02 16:33:06 +0100

    Added content list

--- source/class/mdn/Array.js
+++ source/class/mdn/Array.js
@@ -1,5 +1,5 @@
 /** 
- * Creates a regular expression object for matching text with a @pattern {String} and optional @flags {String}. 
+ * Creates a JavaScript Array.
  * 
  * If the only argument passed to the Array constructor is an integer between 0 and 232-1 (inclusive), 
  * a new JavaScript array is created with @arrayLength {Integer} number of elements. 
