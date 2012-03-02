28230b8b4aa90edec35110b3e00ca4e94cc960c5
Sebastian Werner <info@sebastian-werner.net>
2012-03-02 14:19:07 +0100

    Added new doc helper

---
+++ source/class/mdn/Array.js
@@ -0,0 +1,12 @@
+/** 
+ * Creates a regular expression object for matching text with a @pattern {String} and optional @flags {String}. 
+ * 
+ * If the only argument passed to the Array constructor is an integer between 0 and 232-1 (inclusive), 
+ * a new JavaScript array is created with @arrayLength {Integer} number of elements. 
+ * If the argument is any other number, a RangeError exception is thrown.
+ *
+ * See also: [MDN Documentation](https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Array)
+ *
+ * #main
+ */
+core.Main.declareNamespace("Array", function(arrayLength) {});
