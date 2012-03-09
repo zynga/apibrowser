d43dd6ae1ce0e642c9e3222862e851e3424fe140
Sebastian Werner <swerner@zynga.com>
2012-03-09 15:32:36 +0100

    Another new class

---
+++ source/class/mdn/Element.js
@@ -0,0 +1,14 @@
+/** 
+ * DOM Element. Typically created using `document.createElement()`.
+ * Sub class of {Node}.
+ *
+ * See also [MDN Documentation](https://developer.mozilla.org/en/DOM/element)
+ * 
+ * #main
+ */
+core.Main.addMembers("Element", 
+{
+	nodeType : 1,
+	childNodes : [],
+	children : []
+});
