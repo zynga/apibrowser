7b7663d36029565e5f40331fcdac1acb2091d9d1
Sebastian Werner <swerner@zynga.com>
2012-03-09 15:17:45 +0100

    Another new type spec

---
+++ source/class/custom/CSSClassName.js
@@ -0,0 +1,10 @@
+/** 
+ * Limited {String} used for defining a valid CSS class name. Like a normal {String} but is not allowed to contain:
+ *
+ * - White Space
+ * - Dots
+ *
+ * #main
+ */
+core.Main.declareNamespace("CSSClassName");
+
