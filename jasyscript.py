82f1ddf16085bc0f4fa0d4451797258a2aa8ccfd
Sebastian Werner <swerner@zynga.com>
2012-03-16 17:11:04 +0100

    Cleanups for new jasy

--- jasyscript.py
+++ jasyscript.py
@@ -31,8 +31,7 @@ def distclean():
 def build(dist="build"):
     
     # Write kernel script
-    resolver = Resolver()
-    resolver.addClassName("api.Browser")
+    resolver = Resolver().addClassName("api.Browser")
     assets = Asset(resolver.getIncludedClasses()).exportBuild()
     includedByKernel = storeKernel("%s/script/kernel.js" % dist, assets=assets, debug=False)
 
@@ -65,13 +64,10 @@ def build(dist="build"):
 def source():
     dist = "source"
 
-    # Prepare assets
-    resolver = Resolver()
-    resolver.addClassName("api.Browser")
-    assets = Asset(resolver.getIncludedClasses()).exportSource()
-
     # Write kernel script
-    includedByKernel = storeKernel("%s/script/kernel.js" % dist, assets=assets, formatting=formatting, debug=True)
+    resolver = Resolver().addClassName("api.Browser")
+    assets = Asset(resolver.getIncludedClasses()).exportSource()
+    includedByKernel = storeKernel("%s/script/kernel.js" % dist, assets=assets, debug=True)
 
     # Rewrite template as jsonp
     for tmpl in ["main", "error", "entry", "type", "params", "info", "origin", "tags"]:
@@ -82,13 +78,13 @@ def source():
     for permutation in session.permutate():
 
         # Resolving dependencies
-        resolver = Resolver(permutation)
+        resolver = Resolver()
         resolver.addClassName("api.Browser")
         resolver.excludeClasses(includedByKernel)
 
         # Compressing classes
-        classes = Sorter(resolver, permutation).getSortedClasses()
-        compressedCode = storeSourceLoader("%s/script/browser-%s.js" % (dist, permutation.getChecksum()), classes, bootCode="apibrowser=new api.Browser();")
+        classes = Sorter(resolver).getSortedClasses()
+        compressedCode = storeSourceLoader("%s/script/browser-%s.js" % (dist, getPermutation().getChecksum()), classes, bootCode="apibrowser=new api.Browser();")
 
     # Write API data
     writer = ApiWriter().write("%s/data" % dist, compact=False, callback="apibrowser.callback")
