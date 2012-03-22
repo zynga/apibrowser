7578665c184106fc640cbc15d069deb3106c1d53
Sebastian Werner <swerner@zynga.com>
2012-03-22 15:19:57 +0100

    Added new test classes for native js

---
+++ source/class/test/classic/Class.js
@@ -0,0 +1,26 @@
+/** Creates class for @firstCon {Object} */
+apibrowser.test.classic.Class = function(firstCon) {
+	
+};
+
+/** Some hint for constant PI */
+apibrowser.test.classic.Class.CONSTANT = 3.14;
+
+apibrowser.test.classic.Class.prototype = new apibrowser.test.Food;
+
+apibrowser.test.classic.Class.prototype = {
+	
+	/** Does awesome things */
+	method1 : function() {
+		doSomething();
+	},
+	
+	foobar : true
+	
+};
+
+/** Even more awesomeness */
+apibrowser.test.classic.Class.prototype.method2 = function() {
+	
+};
+
