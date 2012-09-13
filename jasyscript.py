054a2165aa422b92c3b9059f604b3a8023e52b66
Sebastian Werner <swerner@zynga.com>
2012-09-13 15:37:23 +0200

    Adoptions for Jasy 0.8

--- jasyscript.py
+++ jasyscript.py
@@ -40,28 +40,28 @@ def build(theme="original"):
     session.setField("theme", theme)
 
     # Configure assets for being loaded from local asset folder
-    session.getAssetManager().deploy(Resolver().addClassName("apibrowser.Browser").getIncludedClasses())
+    session.getAssetManager().deploy(Resolver(session).addClassName("apibrowser.Browser").getIncludedClasses())
     session.getAssetManager().addBuildProfile()
     
     # Write kernel script
-    includedByKernel = storeKernel("script/kernel.js")
+    includedByKernel = Output(session).storeKernel("script/kernel.js")
 
     # Copy files from source
-    updateFile("source/index.html", "index.html")
+    File.updateFile("source/index.html", "index.html")
     
     # Rewrite template as jsonp
     for tmpl in ["main", "error", "entry", "type", "params", "info", "origin", "tags"]:
         jsonTemplate = json.dumps({ "template" : open("source/tmpl/%s.mustache" % tmpl).read() })
-        writeFile("tmpl/%s.js" % tmpl, "apiload(%s, '%s.mustache')" % (jsonTemplate, tmpl))
+        File.writeFile("tmpl/%s.js" % tmpl, "apiload(%s, '%s.mustache')" % (jsonTemplate, tmpl))
         
     # Process every possible permutation
     for permutation in session.permutate():
         
         # Resolving dependencies
-        resolver = Resolver().addClassName("apibrowser.Browser").excludeClasses(includedByKernel)
+        resolver = Resolver(session).addClassName("apibrowser.Browser").excludeClasses(includedByKernel)
 
         # Compressing classes
-        storeCompressed(resolver.getSortedClasses(), "script/browser-%s.js" % permutation.getChecksum(), "new apibrowser.Browser;")
+        Output(session).storeCompressed(resolver.getSortedClasses(), "script/browser-%s.js" % permutation.getChecksum(), "new apibrowser.Browser;")
 
 
 @task
@@ -74,24 +74,24 @@ def source(theme="original"):
     session.getAssetManager().addSourceProfile()
 
     # Write kernel script
-    includedByKernel = storeKernel("script/kernel.js", debug=True)
+    includedByKernel = Output(session).storeKernel("script/kernel.js", debug=True)
 
     # Rewrite template as jsonp
     for tmpl in ["main", "error", "entry", "type", "params", "info", "origin", "tags"]:
         jsonTemplate = json.dumps({ "template" : open("source/tmpl/%s.mustache" % tmpl).read() })
-        writeFile("tmpl/%s.js" % tmpl, "apiload(%s, '%s.mustache')" % (jsonTemplate, tmpl))
+        File.writeFile("tmpl/%s.js" % tmpl, "apiload(%s, '%s.mustache')" % (jsonTemplate, tmpl))
 
     # Process every possible permutation
     for permutation in session.permutate():
 
         # Resolving dependencies
-        resolver = Resolver().addClassName("apibrowser.Browser").excludeClasses(includedByKernel)
+        resolver = Resolver(session).addClassName("apibrowser.Browser").excludeClasses(includedByKernel)
 
         # Building class loader
-        storeLoader(resolver.getSortedClasses(), "script/browser-%s.js" % permutation.getChecksum(), "new apibrowser.Browser;")
+        Output(session).storeLoader(resolver.getSortedClasses(), "script/browser-%s.js" % permutation.getChecksum(), "new apibrowser.Browser;")
 
     # Generate API data into source folder
-    ApiWriter().write("data")
+    ApiWriter(session).write("data")
 
 
 @task
